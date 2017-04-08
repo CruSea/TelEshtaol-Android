0c96889525215b463143065ca4907e44115c5b8e
amanst1 <amanst1@github.com>
2017-04-08 12:28:03 +0300

    added a java class for the detailed view, managed dependencies

---
+++ app/src/main/java/com/aset/teleshtaol/DetailView.java
@@ -0,0 +1,21 @@
+package com.aset.teleshtaol;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AmanS on 4/8/2017.
+ */
+
+public class DetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feed_detail);
+    }
+
+}
+
+
